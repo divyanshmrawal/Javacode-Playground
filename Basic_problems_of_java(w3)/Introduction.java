e2388e5571d66f68362cb307478772da5d60a22c
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-07-12 14:41:21 +0530

    Create Introduction.java
    Java program to print 'Hello' on screen and your name on a separate line

---
+++ Basic_problems_of_java(w3)/Introduction.java
@@ -0,0 +1,9 @@
+//QUESTION-->
+//Write a Java program to print 'Hello' on screen and your name on a separate line
+
+//ANSWER-->
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
