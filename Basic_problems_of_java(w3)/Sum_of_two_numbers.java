8b1bc3b9fc08150b61deb090c6f059938163de2b
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-07-12 14:51:45 +0530

    Create Sum_of_two_numbers.java
    Java program to print the sum of two numbers.

---
+++ Basic_problems_of_java(w3)/Sum_of_two_numbers.java
@@ -0,0 +1,19 @@
+//QUESTION-->
+//Write a Java program to print the sum of two numbers.
+
+//ANSWER-->
+public class Sum_of_two_numbers{
+ public static void main(String[] args){
+
+    //Creating 2 numbers
+    int num1=3;
+    int num2=2;
+
+    //Sum of 2 numbers
+    int sum=num1+num2;
+
+    //printing result of sum of 2 numbers
+    System.out.println("Sum of two numbers is = "+sum);
+    
+ }
+}
