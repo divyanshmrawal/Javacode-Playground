dfccd1a30e8fc1bd40c7e635c8ccff6bacd8deda
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-07-16 21:30:14 +0530

    Create Print_string_in_a_specific_format_8.java
     Java program to print the string in a specific format

---
+++ Basic_problems_of_java(w3)/Print_string_in_a_specific_format_8.java
@@ -0,0 +1,10 @@
+//QUESTION-->
+//Write a Java program to print the string in a specific format
+
+//ANSWER-->
+public class Print_string_in_a_specific_format_8 {
+
+    public static void main(String[] args) {
+        System.out.println("\nTwinkle,twinkle,little star, \n\t How are wonder what you are! \n\t\t Up above the world so high, \n\t\tLike a diamond in the sky.");
+    }
+}
