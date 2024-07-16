9acac85ed7f895a77ac4ad1c43661e736c95a9a9
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-07-17 00:19:56 +0530

    Create Face_14.java
    java program which prints a face

---
+++ Basic_problems_of_java(w3)/Face_14.java
@@ -0,0 +1,15 @@
+
+//QUESTION-->
+//Write a java program which prints a face
+
+//ANSWER-->
+public class Face_14{
+//Printing a face 
+    public static void main(String[] args){
+        System.out.println("+\"\"\"\"\"\"\"+");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  |");
+        System.out.println(" | '-' |");
+        System.out.println(" +-----+");
+    }
+}
