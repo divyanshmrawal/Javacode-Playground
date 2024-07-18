52b45725101fe5919e827ead4482bad8addd9998
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-07-19 00:31:02 +0530

    Create Swap_two_variables_23.java
    Java program to swap two variables

---
+++ Basic_problems_of_java(w3)/Swap_two_variables_23.java
@@ -0,0 +1,21 @@
+//QUESTION-->
+//Write a Java program to swap two variables
+
+//ANSWER-->
+public class Swap_two_variables_23{
+
+public static void main(String[] args){
+ int num1=2;
+ int num2=3;
+ int x;
+
+System.out.println("Value of num1 and num2 before swaping = "+num1+","+num2);
+
+ x=num1;
+ num1=num2;
+ num2=x;
+
+ System.out.println("Value of num1 and num2 before swaping = "+num1+","+num2);
+
+}
+}
