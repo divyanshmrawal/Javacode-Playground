86f9c14afba37039d07002ab619c1f9c6dab69a8
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-07-20 23:16:39 +0530

    Create Calculative_operations_35.java
    Java program to calculate basic operations

---
+++ Basic_problems_of_java(w3)/Calculative_operations_35.java
@@ -0,0 +1,13 @@
+public class Calculative_operations_35{
+    public static void main(String[] args) {
+        int op1=(6*8)-5;
+        int op2=(55+9)%9;
+        int op3=20+((-3*5)/8);
+        int op4=5+((15/3)*2)-(8%3);
+
+        System.out.println(op1);
+        System.out.println(op2);
+        System.out.println(op3);
+        System.out.println(op4);
+    }
+}
