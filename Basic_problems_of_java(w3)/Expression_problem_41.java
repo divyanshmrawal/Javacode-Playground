1d1c4b5e331f89ef2e35be146ecedba5dd153edb
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-07-21 23:27:48 +0530

    Create Expression_problem_41.java
    java program to calculate basic operators

---
+++ Basic_problems_of_java(w3)/Expression_problem_41.java
@@ -0,0 +1,16 @@
+//QUESTION-->
+//Write a java program to calculate basic operators
+
+//ANSWER-->
+public class Expression_problem_41{
+ public static void main(String[] args) {
+   //Calculation
+    double sum1=(25.5*3.5);
+    double sum2=(3.5*3.5);
+    double sum3=(40.5-4.5);
+    double finalsum=(sum1-sum2)/sum3;
+    
+    //printing result
+    System.out.println(finalsum);
+ }
+}
