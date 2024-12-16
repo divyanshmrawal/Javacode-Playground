09c6ac2ff310d875e0643655915b08293f52317f
Divyansh Rawal <divyanshmrawal@gmail.com>
2024-12-16 23:06:28 +0530

    BankAccount_Class_Implementing_Getters_and_Setters__85.java
    BankAccount_Class_Implementing_Getters_and_Setters__85.java

--- Basic_problems_of_java(w3)/BankAccount_Class_Implementing_Getters_and_Setters__85.java
+++ Basic_problems_of_java(w3)/BankAccount_Class_Implementing_Getters_and_Setters__85.java
@@ -1,7 +1,7 @@
  //QUESTION-->
  //Write a Java program to create a class called BankAccount with private instance variables accountNumber and balance. Provide public getter and setter methods to access and modify these variables.
  
- //ANSWER-->
+ //ANSWER->
  class BankAccount{
     private double accountnumber;
     private double balance;
