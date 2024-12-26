5273b030872acf7c9e84f9220522346910090b7d
Divyansh Rawal <divyanshmrawal@gmail.com>
2024-12-26 22:29:58 +0530

    Majority_Element__26.java
    Majority_Element__26.java

--- DSA_Java/Majority_Element__26.java
+++ DSA_Java/Majority_Element__26.java
@@ -3,7 +3,7 @@
 
 // The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 
-// ANSWER-->
+// ANSWER->
 class Solution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
