536136b8298d7c7a6216052b6a6f8a1dcab7ff31
Divyansh Rawal <divyanshmrawal@gmail.com>
2024-12-30 21:47:23 +0530

    Q5-Middle_of_the_Linked_List.java
    Q5-Middle_of_the_Linked_List.java

--- Linked-List/Q5-Middle_of_the_Linked_List.java
+++ Linked-List/Q5-Middle_of_the_Linked_List.java
@@ -1,4 +1,4 @@
-//QUESTION-->
+//QUESTION->
 //Given the head of a singly linked list, return the middle node of the linked list. If there are two middle nodes, return the second middle node.
 
  //Example 1:
