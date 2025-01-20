d4e019e30c9d605f4dab358288783b66d1fe9be6
Divyansh Rawal <divyanshmrawal@gmail.com>
2025-01-20 16:20:26 +0530

    Q4-Remove_Linked_List_Elements.java
    Q4-Remove_Linked_List_Elements.java

--- Linked-List/Q4-Remove_Linked_List_Elements.java
+++ Linked-List/Q4-Remove_Linked_List_Elements.java
@@ -1,4 +1,4 @@
-//QUESTION-->
+//QUESTION->
 // //Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
 
 // Example 1:
