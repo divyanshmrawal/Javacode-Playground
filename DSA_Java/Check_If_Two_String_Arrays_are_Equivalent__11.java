52777055fccfc615774322ee34ff7e055d4d0dfd
Divyansh Rawal <divyanshmrawal@gmail.com>
2025-01-30 23:28:29 +0530

    Check_If_Two_String_Arrays_are_Equivalent__11.java
    Check_If_Two_String_Arrays_are_Equivalent__11.java

--- DSA_Java/Check_If_Two_String_Arrays_are_Equivalent__11.java
+++ DSA_Java/Check_If_Two_String_Arrays_are_Equivalent__11.java
@@ -1,9 +1,9 @@
-//QUESTION->
+//QUESTION-->
 //Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
 
 // A string is represented by an array if the array elements concatenated in order forms the string.
 
-//ANSWER->
+//ANSWER-->
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         StringBuilder st1=new StringBuilder();
