8ad9aba9c7880ee1d517b6e22ebd28a7f2adac9e
Divyansh Rawal <divyanshmrawal@gmail.com>
2025-02-02 19:46:55 +0530

    Count-Asterisks-55.java
    Count-Asterisks-55.java

--- DSA_Java/Count-Asterisks-55.java
+++ DSA_Java/Count-Asterisks-55.java
@@ -1,4 +1,4 @@
-//QUESTION-->
+//QUESTION->
 //You are given a string s, where every two consecutive vertical bars '|' are grouped into a pair. In other words, the 1st and 2nd '|' make a pair, the 3rd and 4th '|' make a pair, and so forth.
 
 // Return the number of '*' in s, excluding the '*' between each pair of '|'.
